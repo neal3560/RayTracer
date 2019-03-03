e96e637e4aea0a55b80788c5a753783776a872ce
neal3560 <weh073@ucsd.edu>
2019-03-03 15:52:07 -0800

    1.0

---
+++ RayTracer/RayTracer/main.cpp
@@ -0,0 +1,11 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	glm::vec3(0, 1, 1);
+}
